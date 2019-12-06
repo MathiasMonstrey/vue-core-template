c83e80c678fb2cc0e71aa17b7e2145b93c91c987
Mathias Monstrey <me@mathiasmonstrey.be>
2019-12-06 22:44:25 +0100

    renaming base router and store

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import app from "./base-module/app";
-import router from "./base-module/router-base";
-import store from "./base-module/store-base";
+import router from "./base-module/base-router";
+import store from "./base-module/base-store";
 
 Vue.config.productionTip = false;
 
